d553f83620fe5864be49089c46920b015df40127
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-07-18 15:44:51 +0530

    Updates

---
+++ precache-manifest.91b69eabbf8403baab6933d81719d639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5113e900ff751a979cbe480afde5c0",
+    "url": "/machineslot/index.html"
+  },
+  {
+    "revision": "6ee87ffa11fe8978029c",
+    "url": "/machineslot/static/css/main.d3aa593b.chunk.css"
+  },
+  {
+    "revision": "28698757616be9a21375",
+    "url": "/machineslot/static/js/2.254e522b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/machineslot/static/js/2.254e522b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee87ffa11fe8978029c",
+    "url": "/machineslot/static/js/main.95cc5830.chunk.js"
+  },
+  {
+    "revision": "911e353e351b5098eee1",
+    "url": "/machineslot/static/js/runtime-main.cb96b049.js"
+  }
+]);
